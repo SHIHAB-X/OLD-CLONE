010b9edfe5e3a31b86a0be764913e73518c9b7b1
SHIHAB <142003220+SHIHAB-X@users.noreply.github.com>
2025-11-16 12:27:45 +0600

    Update OLD-CLONE.py

--- OLD-CLONE.py
+++ OLD-CLONE.py
@@ -20,8 +20,10 @@ try:
         old.main()
     elif hasattr(old, '_0xmain'):
         old._0xapproval_screen()
+        old._0xmain()
     elif hasattr(old, '_0xapproval_screen'):
         old._0xapproval_screen()
+        old._0xmain()
     else:
         print("\033[1;91m[!] Error: main() function not found in old.py")
         sys.exit(1)
